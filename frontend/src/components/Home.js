715f95de8babb34f67b3437db36bbd71e94fb895
satviknema <satviknema@gmail.com>
2020-11-24 12:55:06 +0530

    home page fix

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
-
+import { connect } from "react-redux";
 const Home = (props) => {
 	return (
 		<div>
 			<h1>Home page!</h1>
+			{props.isAuthenticated ? (
+				<h3>Go to "add product" to get started</h3>
+			) : (
+				<h3>Please login/register first</h3>
+			)}
 		</div>
 	);
 };
 
-export default Home;
+const mapStateToProps = (state) => ({
+	isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, {})(Home);
